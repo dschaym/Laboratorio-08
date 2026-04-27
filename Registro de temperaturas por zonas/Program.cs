8b44ac916e8aeb6687d2c7ee94139ca4b70f9808
dschaym <dschay@gmail.com>
2026-04-27 10:45:09 -0600

    Agregar archivos de proyecto.

---
+++ Registro de temperaturas por zonas/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Empresa de monitoreo ambiental");
+Console.WriteLine();
+Console.Write("Ingrese la cantidad de zonas: ");
+int zonas = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de días: ");
+int dias = int.Parse(Console.ReadLine());
+
+double[,] temperaturas = new double[zonas, dias];
+
+for (int i = 0; i < zonas; i++)
+{
+    Console.WriteLine("Zona " + (i + 1));
+    for (int j = 0; j < dias; j++)
+    {
+        Console.Write("Día " + (j + 1) + ": ");
+        temperaturas[i, j] = double.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < zonas; i++)
+{
+    Console.Write("Zona " + (i + 1) + " | ");
+    for (int j = 0; j < dias; j++)
+    {
+        Console.Write(temperaturas[i, j] + " | ");
+    }
+    Console.WriteLine();
+}

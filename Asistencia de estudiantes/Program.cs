1960c467cef0f042e9406b0b48c12f89365ced50
dschaym <dschay@gmail.com>
2026-04-27 11:43:02 -0600

    Ejercicios

---
+++ Asistencia de estudiantes/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Asistencia de estudiantes");
+Console.WriteLine();
+Console.Write("Ingrese número de estudiantes: ");
+int estudiantes = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese número de días: ");
+int dias = int.Parse(Console.ReadLine());
+
+int[,] asistencia = new int[estudiantes, dias];
+
+for (int i = 0; i < estudiantes; i++)
+{
+    for (int j = 0; j < dias; j++)
+    {
+        Console.Write("Asistencia: ");
+        asistencia[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Matriz de asistencia:");
+for (int i = 0; i < estudiantes; i++)
+{
+    for (int j = 0; j < dias; j++)
+    {
+        Console.Write(asistencia[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Seleccione estudiante: ");
+int est = int.Parse(Console.ReadLine()) - 1;
+
+for (int j = 0; j < dias; j++)
+{
+    Console.WriteLine(asistencia[est, j]);
+}
---
+++ Producción de una fábrica/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Cantidad de productos fabricados, fábrica FACSSA");
+Console.WriteLine();
+Console.Write("Ingrese número de máquinas: ");
+int maquinas = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese número de turnos: ");
+int turnos = int.Parse(Console.ReadLine());
+
+int[,] produccion = new int[maquinas, turnos];
+int total = 0;
+
+for (int i = 0; i < maquinas; i++)
+{
+    for (int j = 0; j < turnos; j++)
+    {
+        Console.Write("Producción: ");
+        produccion[i, j] = int.Parse(Console.ReadLine());
+        total = total + produccion[i, j];
+    }
+}
+
+Console.WriteLine("Matriz de producción:");
+for (int i = 0; i < maquinas; i++)
+{
+    for (int j = 0; j < turnos; j++)
+    {
+        Console.Write(produccion[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Producción total: " + total);

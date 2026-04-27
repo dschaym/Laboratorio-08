2a15e9a9ace6b1f900dd97a6f64b11cb8bd56a50
dschaym <dschay@gmail.com>
2026-04-27 11:03:02 -0600

    Ejercicios

---
+++ Consumo eléctrico total/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Consumo de distintos apartamentos");
+Console.WriteLine();
+Console.Write("Ingrese el número de apartamentos: ");
+int apartamentos = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el número de días: ");
+int dias = int.Parse(Console.ReadLine());
+
+double[,] consumo = new double[apartamentos, dias];
+double total = 0;
+
+for (int i = 0; i < apartamentos; i++)
+{
+    for (int j = 0; j < dias; j++)
+    {
+        Console.Write("Consumo del apartamento " + (i + 1) + " dia " + (j + 1) + ": ");
+        consumo[i, j] = double.Parse(Console.ReadLine());
+        total += consumo[i, j];
+    }
+}
+Console.WriteLine("Consumo total: " + total);
---
+++ Ventas de productos por día/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Negocio en registro de productos");
+Console.WriteLine();
+Console.Write("Ingrese el número de productos: ");
+int productos = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el número de días: ");
+int dias = int.Parse(Console.ReadLine());
+
+int[,] ventas = new int[productos, dias];
+
+for (int i = 0; i < productos; i++)
+{
+    Console.WriteLine("Producto " + (i + 1));
+    for (int j = 0; j < dias; j++)
+    {
+        Console.Write("Día " + (j + 1) + ": ");
+        ventas[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine();
+Console.Write("Ingrese el número del producto que desea ver: ");
+int productoElegido = int.Parse(Console.ReadLine());
+
+productoElegido = productoElegido - 1;
+
+Console.WriteLine();
+Console.WriteLine("Ventas del producto " + (productoElegido + 1));
+
+for (int j = 0; j < dias; j++)
+{
+    Console.WriteLine("Día " + (j + 1) + ": " + ventas[productoElegido, j]);
+}
